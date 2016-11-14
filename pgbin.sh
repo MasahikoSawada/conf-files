4e88e7b9385a3921c175b18a393da36581a3d03b
Masahiko Sawada <sawada.mshk@gmail.com>
2016-11-14 11:33:55 +0900

    Support p and pp command.

--- pgbin.sh
+++ pgbin.sh
@@ -1,14 +1,17 @@
+#!/bin/bash
 PGBASE=/home/masahiko/pgsql
 
 # Available functions are;
 # - start [version]
 # - restart [version]
 # - stop [version
-# - install [version]
+# - install_pg [version]
 # - init [version]
 # - full_setup [version]
 # - sync_rep [version] [num]
 # - rebuld [version] [options]
+# - pp { [version] ...} [options for psql including -c option]
+# - p [version] [connection option for psql]
 # - list
 
 function error()
@@ -18,48 +21,38 @@ function error()
 
 function start()
 {
-    VERSION=$1
+    VERSION=`s_to_version $1`
+    PORT=`s_to_port $1`
     P=`pwd`
     cd $PGBASE/$VERSION
 
-    if [ "$VERSION" == "master" ];then
-	PORT=5432
-    else
-	PORT=5`echo $VERSION | tr -d "."`
-    fi
-
-    bin/pg_ctl start -D data -o "-p $PORT"
+    bin/pg_ctl start -D data -o "-p $PORT" -c
     cd $P
 }
 
 function restart()
 {
-    VERSION=$1
+    VERSION=`s_to_version $1`
+    PORT=`s_to_port $1`
     P=`pwd`
     cd $PGBASE/$VERSION
 
-    if [ "$VERSION" == "master" ];then
-	PORT=5432
-    else
-	PORT=5`echo $VERSION | tr -d "."`
-    fi
-
-    bin/pg_ctl restart -D data -mf
+    bin/pg_ctl restart -D data -mf -c
     cd $P
 }
 
 function stop()
 {
-    VERSION=$1
+    VERSION=`s_to_version $1`
     P=`pwd`
     cd $PGBASE/$VERSION
     bin/pg_ctl stop -D data -mf
     cd $P
 }
 
-function install()
+function install_pg()
 {
-    VERSION=$1
+    VERSION=`s_to_version $1`
     P=`pwd`
 
     EXISTS=`ls -1 $PGBASE/$VERSION`
@@ -77,7 +70,7 @@ function install()
 
 function init()
 {
-    VERSION=$1
+    VERSION=`s_to_version $1`
     P=`pwd`
 
     cd $PGBASE/$VERSION
@@ -89,7 +82,7 @@ function init()
 
 function full_setup()
 {
-    VERSION=$1
+    VERSION=`s_to_version $1`
     ORIG_PWD=`pwd`
 
     install $VERSION
@@ -106,7 +99,7 @@ function full_setup()
 
 function rebuild()
 {
-    VERSION=$1
+    VERSION=`s_to_version $1`
     shift
     OPTIONS=$@
     ORIG_PWD=`pwd`
@@ -119,6 +112,106 @@ function rebuild()
     make install -j 4 -s
 
     cd ${ORIG_PWD}
+}
+
+function s_to_version()
+{
+    if [ "$1" != "master" ];then
+	VERSION_1=`echo $1 | cut -b1`
+	VERSION_2=`echo $1 | cut -b2`
+	VERSION_3=`echo $1 | cut -b3-`
+	echo "${VERSION_1}.${VERSION_2}.${VERSION_3}"
+	return
+    else
+	echo "master"
+	return
+    fi
+}
+
+function s_to_port()
+{
+    if [ "$1" != "master" ];then
+	echo "5$1"
+	return
+    else
+	echo "5432"
+	return
+    fi
+}
+
+function pp()
+{
+    options=""
+    versions=()
+    for OPT in "$@"
+    do
+	if [ "$before_command" == "true" ];then
+	    command="\"$OPT\""
+	    before_command="false"
+	    continue
+	fi
+
+	case $OPT in
+	    [0-9][0-9][0-9] |  [0-9][0-9][0-9][0-9] | 'master')
+		versions=("${versions[@]}" "$OPT")
+		shift
+		;;
+	    '-c')
+		command_opt="-c"
+		before_command="true"
+		shift
+		;;
+	    *)
+		options="$options $OPT"
+		;;
+	esac
+    done
+
+    if [ "$versions" == "" ];then
+	versions=("master")
+    fi
+
+    for version in "${versions[@]}"
+    do
+	VERSION=`s_to_version $version`
+	PORT=`s_to_port $version`
+
+	echo "==== $VERSION ===="
+	c="$PGBASE/$VERSION/bin/psql -d postgres -p $PORT $options $command_opt $command"
+	eval "$c"
+    done
+}
+
+function p()
+{
+    for OPT in "$@"
+    do
+	if [ "$before_command" == "true" ];then
+	    command="\"$OPT\""
+	    before_command="false"
+	    continue
+	fi
+
+	case $OPT in
+	    [0-9][0-9][0-9] |  [0-9][0-9][0-9][0-9] | 'master')
+		version="$OPT"
+		shift
+		;;
+	    '-c')
+		command_opt="-c"
+		before_command="true"
+		shift
+		;;
+	    *)
+		options="$options $OPT"
+		;;
+	esac
+    done
+
+    VERSION=`s_to_version $version`
+    PORT=`s_to_port $version`
+
+    $PGBASE/$VERSION/bin/psql -d postgres -p $PORT "$options"
 }
 
 function sync_rep()
